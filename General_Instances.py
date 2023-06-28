61a7ae4d8b17517b1e6e5fca6f4b473469c78f49
Rahmi Ata Turan <r.a.turan@student.vu.nl>
2023-06-28 16:39:31 +0300

    General Instances for the Vrije Universiteit

--- General_Instances.py
+++ General_Instances.py
@@ -1,1 +1,7 @@
 #Instance level
+#Vrije is of type Uni
+# VuTemplate1.7 is of DMPTemplate
+#VuTemplate1.7 consists_of Sections
+# Sections consists_of TemplateQuestions
+# TemplateQuestions hasTitle questionitself.
+# TemplateQuestions of type DMPQuestions
